5ad75a466db9ea9f8809ef9bd06b88c2d7a2396b
Steven Leal <steven.leal@unity3d.com>
2020-08-25 15:45:53 -0500

    added random parameters scriptable object

---
+++ com.unity.perception/Editor/Randomization/Editors/RandomParametersAssetEditor.cs
@@ -0,0 +1,29 @@
+﻿using Randomization.ParameterBehaviours;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace UnityEngine.Perception.Randomization.Editor
+{
+    [CustomEditor(typeof(RandomParametersAsset), true)]
+    public class RandomParametersAssetEditor : UnityEditor.Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = new VisualElement();
+            var iterator = serializedObject.GetIterator();
+
+            // Start iterating over properties
+            iterator.NextVisible(true);
+
+            // Skip m_Script property
+            iterator.NextVisible(false);
+
+            // Create property fields
+            do { root.Add(new PropertyField(iterator.Copy())); }
+            while (iterator.NextVisible(false));
+
+            return root;
+        }
+    }
+}
---
+++ com.unity.perception/Runtime/Randomization/ParameterBehaviours/RandomParametersAsset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Perception.Randomization.Parameters;
+
+namespace Randomization.ParameterBehaviours
+{
+    public abstract class RandomParametersAsset : ScriptableObject
+    {
+        public abstract IEnumerable<Parameter> parameters { get; }
+    }
+}
